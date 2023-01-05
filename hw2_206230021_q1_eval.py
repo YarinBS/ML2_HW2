c2e4253623a8ae16053dd6b05711662a4381f4d3
YarinBS <yarin10121@gmail.com>
2023-01-06 00:23:15 +0200

    The magic command model.eval()

--- hw2_206230021_q1_eval.py
+++ hw2_206230021_q1_eval.py
@@ -1,17 +1,27 @@
 import torch
-from hw2_206230021_q1_train import fetch_CIFAR10_data
+import random
+import os
+import numpy as np
+from hw2_206230021_q1_train import fetch_CIFAR10_data, MyConvNet
 
 
-def evaluate_model_q1() -> float:
+def evaluate_model_q1():
     """
     Loads CIFAR10's test set and the trained CNN network, and returns the average error on the test set
     :return: Average error rate
     """
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
 
     _, _, test_data, test_loader = fetch_CIFAR10_data()
-    model = torch.load("q1_model.pkl")
 
+    model = MyConvNet()
+    if torch.cuda.is_available():
+        model.load_state_dict(torch.load('q1_model.pkl'))
+    else:
+        model.load_state_dict(torch.load('q1_model.pkl', map_location=torch.device('cpu')))
+
+    model.eval()
     total, correct = 0, 0
     for (test_images, test_labels) in test_loader:
         test_images = test_images.to(device)
@@ -21,12 +31,25 @@ def evaluate_model_q1() -> float:
         total += test_labels.size(0)
         correct += (test_predictions == test_labels).sum().item()
 
+    acc = 100 * correct / total
     error = 100 * (1 - (correct / total))
-    print(f"{round(error, 3)}")
+    print(f"Accuracy: {acc}, Error: {error}%")
     return error
 
 
 def main():
+    # --- Seed for reproducibility ------
+    seed = 1
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    # ------------------------------------
+
     evaluate_model_q1()
 
 
